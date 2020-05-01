7857a9857f176699b1db5065cec8044d10fc71d5
jubao <tqy_4896@hotmail.com>
2020-05-01 21:45:42 +0800

    Initial Commit

---
+++ hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw3
+//
+//  Created by User16 on 2020/5/1.
+//  Copyright © 2020 User16. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
